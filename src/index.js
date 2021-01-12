a1d514b25cd5ef8d92541a477de83369bf585427
Mahesh Kaware <kawaremahesh7@gmail.com>
2021-01-12 10:27:57 +0530

    Commit done for list with functional component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 
+const lakeList=[
+  "Echo lake",
+  "Maud Lake",
+  "Cascade lake"
+];
 
+function Hello({ lake }){
+  return(
+    <div>
+      <h1>Hello from functional component...</h1>
+     <ul>
+      {lake.map(
+        item => (<li>{item}</li>)
+      )}
+     </ul>
+    </div>
+  );
+}
 // let city={
 //   name:"Pune",
 //   state:"Maharashtra"
 // };
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+
+<Hello lake={lakeList}/>,
   // React.createElement("h1", {style:{color:"blue"}},"Hello"),
   // <h2>{city.name} is in {city.state}   !!</h2>,
   document.getElementById('root')
