f9aa569ce6ba096faa5da04ad9a27f6ffc73446b
Mahesh Kaware <kawaremahesh7@gmail.com>
2021-01-12 12:44:22 +0530

    useState and useEffect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
@@ -8,6 +8,8 @@ const lakeList=[
   "Maud Lake",
   "Cascade lake"
 ];
+
+
 
 // function Hello({ season }){
 //   return(
@@ -22,31 +24,52 @@ const lakeList=[
 //   );
 // }
 
+// function Hello({ season }){
+//   return(
+//     <div>
+//       {(season == "summer") ? <Summer /> : <Winter />}
+//     </div>
+//   );
+// }
+
+
+
+// function Hello({ season }){
+//   const [status,setStatus]=useState("Open");
+//   return(
+//     <div>
+//      <h1> Status : { status }</h1>
+//      <button onClick={() => setStatus("closed")}>Close</button>
+//      <button onClick={() => setStatus("In-progress")}>In-progress</button>
+//     </div>
+//   );
+// }
+
 function Hello({ season }){
+  const [val,setVal]=useState("");
+  const [val2,setVal2] = useState("");
+
+  useEffect(
+   () =>{
+     console.log(`VAL_1 : ${val}`);
+   },[val]);
+  
+
+  useEffect(()=>{
+    console.log(`VAL_2 : ${val2}`);
+  },[val2]);
+
   return(
     <div>
-      {(season == "summer") ? <Summer /> : <Winter />}
+      <label>Fav Place 1</label>
+      <input value={val} onChange={(e) => setVal(e.target.value)}></input>
+
+      <label>Fav Place 2</label>
+      <input value={val2} onChange={(e) => setVal2(e.target.value)}></input>
     </div>
   );
 }
 
-function  Winter() {
-  return(
-    <div>
-      <h2>Welcome to winter...</h2>
-    </div>
-  );
-  
-}
-
-function Summer() {
-  return(
-    <div>
-      <h2> Welcomw to summer...</h2>
-    </div>
-  );
-  
-}
 
 
 ReactDOM.render(
@@ -54,7 +77,7 @@ ReactDOM.render(
   //   <App />
   // </React.StrictMode>,
 
-<Hello season="winter"/>,
+<Hello />,
   document.getElementById('root')
 );
 
